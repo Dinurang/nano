7a52446ff8d084739fa345dcf8240316b10613e4
Dinurang <dinura1ginige@gmail.com>
2025-05-09 13:16:22 +0530

    Update Reg.vhd

--- RegisterBank.srcs/sources_1/new/Reg.vhd
+++ RegisterBank.srcs/sources_1/new/Reg.vhd
@@ -11,53 +11,18 @@ end Reg;
 
 architecture Behavioral of Reg is
 
-component D_FF
-port (     D : in STD_LOGIC;
-           Res : in STD_LOGIC;
-           Clk : in STD_LOGIC;
-           Q : out STD_LOGIC;
-           Qbar : out STD_LOGIC);
-end component;
-
-signal y0,y1,y2,y3 : std_logic;
 begin
 
-D_FF_0 : D_FF 
-port map(
-      D => I(0),
-      Res => RegReset,
-      Clk => RegClock,
-      Q => y0);
-
-D_FF_1 : D_FF 
-port map(
-     D => I(1),
-      Res => RegReset,
-      Clk => RegClock,
-      Q => y1);
-
-D_FF_2 : D_FF 
-port map(
-      D => I(2),
-      Res => RegReset,
-      Clk => RegClock,
-      Q => y2);
-
-D_FF_3 : D_FF 
-port map(
-  D => I(3),
-      Res => RegReset,
-      Clk => RegClock,
-      Q => y3);
-
-process (RegEn, y0, y1, y2, y3)
-begin
-    if RegEn = '1' then
-        Y <= (y3 & y2 & y1 & y0); -- Concatenate bits into a vector
-    else
-        Y <= "0000"; -- Assign the full vector
-    end if;
+  process(RegClock) begin
+         if (rising_edge(RegClock)) then -- respond when clock rises
+         if RegReset = '1' then
+            Y <= "0000";
+         elsif RegEn = '1' then -- Enable should be set
+            Y <= I;
+         end if;
+     end if;
 end process;
+
 
 
 
